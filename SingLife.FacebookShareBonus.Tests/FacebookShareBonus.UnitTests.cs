b238c63645616cdc07e0a2d0960a46d009355aa1
CONTEMIVIETNAM\lukas.nguyen <lukas.nguyen@contemi.com.vn>
2018-01-02 16:42:26 +0700

    First commit

---
+++ SingLife.FacebookShareBonus.Tests/FacebookShareBonus.UnitTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using SingLife.FacebookShareBonus.Model;
+
+namespace SingLife.FacebookShareBonus.Tests
+{
+    /// <summary>
+    /// Summary description for FacebookBonus
+    /// </summary>
+    [TestFixture]
+    public class FacebookShareBonus
+    {
+        public FacebookShareBonus()
+        {
+        }
+        public class Initialize
+        {
+            public static FacebookBonusCalculationInput GetListSortingByDateWithSmallBonusOfPoint()
+            {
+                Policy policy1 = new Policy { PolicyNumber = "P001", Premium = 200, StartDate = new DateTime(2016, 5, 12) };
+                Policy policy2 = new Policy { PolicyNumber = "P002", Premium = 100, StartDate = new DateTime(2017, 11, 8) };
+                Policy[] policies = new Policy[] { policy1, policy2 };
+                FacebookBonusSettings facebookBonusSettings = new FacebookBonusSettings { BonusPercentage = 0.03F, MaximumBonus = 10 };
+                return new FacebookBonusCalculationInput { PoliciesOfCustomer = policies, Settings = facebookBonusSettings };
+            }
+            public static FacebookBonusCalculationInput GetListSortingByDate()
+            {
+                Policy policy1 = new Policy { PolicyNumber = "P001", Premium = 200, StartDate = new DateTime(2016, 5, 12) };
+                Policy policy2 = new Policy { PolicyNumber = "P002", Premium = 300, StartDate = new DateTime(2017, 11, 8) };
+                Policy[] policies = new Policy[] { policy1, policy2 };
+                FacebookBonusSettings facebookBonusSettings = new FacebookBonusSettings { BonusPercentage = 0.03F, MaximumBonus = 10 };
+                return new FacebookBonusCalculationInput { PoliciesOfCustomer = policies, Settings = facebookBonusSettings };
+            }
+            public static FacebookBonusCalculationInput GetListNotSortingByDate()
+            {
+                Policy policy2 = new Policy { PolicyNumber = "P002", Premium = 300, StartDate = new DateTime(2017, 11, 8) };
+                Policy policy1 = new Policy { PolicyNumber = "P001", Premium = 200, StartDate = new DateTime(2016, 5, 12) };
+                Policy[] policies = new Policy[] { policy1, policy2 };
+                FacebookBonusSettings facebookBonusSettings = new FacebookBonusSettings { BonusPercentage = 0.03F, MaximumBonus = 10 };
+                return new FacebookBonusCalculationInput { PoliciesOfCustomer = policies, Settings = facebookBonusSettings };
+            }
+        }
+            //
+            // TODO: Add constructor logic here
+            //
+        [Test]
+        public void FacebookBonus_BonusInPointLargerThanMaximumBonus_Total()
+        {
+          
+            FacebookBonusCalculator facebookBonusCalculator = new FacebookBonusCalculator();
+            var output = facebookBonusCalculator.Calculate(Initialize.GetListSortingByDate());
+
+            PolicyBonus policyBonus1 = new PolicyBonus { PolicyNumber = "P001", BonusInPoints = 6 };
+            PolicyBonus policyBonus2 = new PolicyBonus { PolicyNumber = "P002", BonusInPoints = 4 };
+            PolicyBonus[] policyBonuses = new PolicyBonus[] { policyBonus1, policyBonus2 };
+            FacebookBonus expected = new FacebookBonus { PolicyBonuses = policyBonuses };
+
+            NUnit.Framework.Assert.AreEqual(expected.Total, output.Total);
+        }
+        [Test]
+        public void FacebookBonus_BonusInPointSmallerThanMaximumBonus_Total()
+        {
+           
+            FacebookBonusCalculator facebookBonusCalculator = new FacebookBonusCalculator();
+            var output = facebookBonusCalculator.Calculate(Initialize.GetListSortingByDateWithSmallBonusOfPoint());
+
+            PolicyBonus policyBonus1 = new PolicyBonus { PolicyNumber = "P001", BonusInPoints = 6 };
+            PolicyBonus policyBonus2 = new PolicyBonus { PolicyNumber = "P002", BonusInPoints = 3 };
+            PolicyBonus[] policyBonuses = new PolicyBonus[] { policyBonus1, policyBonus2 };
+            FacebookBonus expected = new FacebookBonus { PolicyBonuses = policyBonuses };
+
+            NUnit.Framework.Assert.AreEqual(expected.Total, output.Total);
+        }
+        [Test]
+        public void FacebookBonus_BonusInPointLargerThanMaximumBonus_ListPolicyBonus()
+        {
+            FacebookBonusCalculator facebookBonusCalculator = new FacebookBonusCalculator();
+            var output = facebookBonusCalculator.Calculate(Initialize.GetListSortingByDate());
+
+            PolicyBonus policyBonus1 = new PolicyBonus { PolicyNumber = "P001", BonusInPoints = 6 };
+            PolicyBonus policyBonus2 = new PolicyBonus { PolicyNumber = "P002", BonusInPoints = 4 };
+            PolicyBonus[] policyBonuses = new PolicyBonus[] { policyBonus1, policyBonus2 };
+            FacebookBonus expected = new FacebookBonus { PolicyBonuses = policyBonuses };
+
+            NUnit.Framework.Assert.AreEqual(expected.ToString(), output.ToString());
+        }
+        [Test]
+        public void FacebookBonus_BonusInPointSmallerThanMaximumBonus_ListPolicyBonus()
+        {
+            FacebookBonusCalculator facebookBonusCalculator = new FacebookBonusCalculator();
+            var output = facebookBonusCalculator.Calculate(Initialize.GetListSortingByDateWithSmallBonusOfPoint());
+
+            PolicyBonus policyBonus1 = new PolicyBonus { PolicyNumber = "P001", BonusInPoints = 6 };
+            PolicyBonus policyBonus2 = new PolicyBonus { PolicyNumber = "P002", BonusInPoints = 3 };
+            PolicyBonus[] policyBonuses = new PolicyBonus[] { policyBonus1, policyBonus2 };
+            FacebookBonus expected = new FacebookBonus { PolicyBonuses = policyBonuses };
+
+            NUnit.Framework.Assert.AreEqual(expected.ToString(), output.ToString());
+        }
+        [Test]
+        public void FacebookBonus_ListPolicyNotSortingByDate_ListPolicyBonus()
+        {
+            FacebookBonusCalculator facebookBonusCalculator = new FacebookBonusCalculator();
+            var output = facebookBonusCalculator.Calculate(Initialize.GetListNotSortingByDate());
+
+            PolicyBonus policyBonus1 = new PolicyBonus { PolicyNumber = "P001", BonusInPoints = 6 };
+            PolicyBonus policyBonus2 = new PolicyBonus { PolicyNumber = "P002", BonusInPoints = 4 };
+            PolicyBonus[] policyBonuses = new PolicyBonus[] { policyBonus1, policyBonus2 };
+            FacebookBonus expected = new FacebookBonus { PolicyBonuses = policyBonuses };
+
+            NUnit.Framework.Assert.AreEqual(expected.ToString(), output.ToString());
+        }
+    }
+}
---
+++ SingLife.FacebookShareBonus.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SingLife.FacebookShareBonus.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SingLife.FacebookShareBonus.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b304e99c-bc34-468f-b333-f43eea67469b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
