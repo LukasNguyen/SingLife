2e197fc6712f0246c2457cc5de1be2ba4f4c24ac
CONTEMIVIETNAM\lukas.nguyen <lukas.nguyen@contemi.com.vn>
2018-01-03 17:10:34 +0700

    refactor expected result

---
+++ SingLife.FacebookShareBonus.Model/FacebookBonusCalculator.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SingLife.FacebookShareBonus.Model
+{
+    ///<summary>
+    /// /// A domain service that calculates the bonus rewarded to a customer who has shared to Facebook
+    /// ///</summary>
+    public class FacebookBonusCalculator
+    {
+        /// <summary>
+        ///  Calculates the bonus.
+        /// </summary>
+        /// <param name="input">A parameter object representing inputs to the calculation.</param>
+        /// <returns>A <see cref="FacebookBonus"/> object.</returns>
+        public virtual FacebookBonus Calculate(FacebookBonusCalculationInput input)
+        {
+            // TODO: Implement bonus calculation.
+
+            var facebookBonusSetting = input.Settings;
+            int sumBonusInPoints = 0;
+
+            var lstPolicies = input.PoliciesOfCustomer.OrderBy(n => n.StartDate);
+
+            List<PolicyBonus> lstPolicyBonuses = new List<PolicyBonus>();
+
+            foreach (var item in lstPolicies)
+            {
+                float bonusInPoints = (float)item.Premium * facebookBonusSetting.BonusPercentage;
+                int bonusInPointsAfterFloor = (int)Math.Floor(bonusInPoints);
+
+                sumBonusInPoints += bonusInPointsAfterFloor;
+
+                PolicyBonus policyBonus = new PolicyBonus
+                {
+                    PolicyNumber = item.PolicyNumber,
+                    BonusInPoints = bonusInPointsAfterFloor
+                };
+
+                lstPolicyBonuses.Add(policyBonus);
+                if (sumBonusInPoints > facebookBonusSetting.MaximumBonus)
+                {
+                    var temp = lstPolicyBonuses.ToList();
+                    temp.RemoveAt(temp.Count - 1);
+                    lstPolicyBonuses.Last().BonusInPoints = (int)facebookBonusSetting.MaximumBonus - temp.Sum(n => n.BonusInPoints);
+
+                    return new FacebookBonus
+                    {
+                        PolicyBonuses = lstPolicyBonuses.ToArray()
+                    };
+                }
+            }
+            return new FacebookBonus
+            {
+                PolicyBonuses = lstPolicyBonuses.ToArray()
+            };
+        }
+    }
+
+    /// <summary>
+    /// Parameter class of the <see cref="FacebookBonusCalculator.Calculate(FacebookBonusCalculationInput)"/> method.
+    /// </summary>
+    public class FacebookBonusCalculationInput
+    {
+        /// <summary>
+        /// A collection of policies owned by the customer.
+        /// </summary>
+        public Policy[] PoliciesOfCustomer { get; set; }
+
+        /// <summary>
+        /// Facebook bonus calculation settings.
+        /// </summary>
+        public FacebookBonusSettings Settings { get; set; }
+    }
+
+    public class Policy
+    {
+        public string PolicyNumber { get; set; }
+        public decimal Premium { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+
+    public class FacebookBonusSettings
+    {
+        public float BonusPercentage { get; set; }
+        public decimal MaximumBonus { get; set; }
+        public IPolicySortService PolicySorter { get; set; }
+    }
+
+    /// <summary>
+    /// Contract of a policy-sorting service.
+    /// </summary>
+    public interface IPolicySortService
+    {
+        /// <summary>
+        //// Sorts the specified policies.
+        /// </summary>
+        /// <param name="policies">An enumerable of policies to be sorted.</param>
+        /// <returns>An ordered enumerable of policies.</returns>
+        IEnumerable<Policy> Sort(IEnumerable<Policy> policies);
+    }
+
+    /// <summary>
+    /// Represents the bonus rewarded to a customer for sharing to Facebook.
+    /// </summary>
+    public class FacebookBonus
+    {
+        public PolicyBonus[] PolicyBonuses { get; set; }
+
+        public int Total
+        {
+            get
+            {
+                // TODO: Implement bonus total calculation.
+                return PolicyBonuses.Sum(n => n.BonusInPoints);
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            foreach (var item in PolicyBonuses)
+            {
+                s += item.PolicyNumber + " ";
+            }
+            return s + Total.ToString();
+        }
+    }
+
+    public class PolicyBonus
+    {
+        public string PolicyNumber { get; set; }
+        public int BonusInPoints { get; set; }
+    }
+
+    //public class MyPolicySort : IPolicySortService
+    //{
+    //    public IEnumerable<Policy> Sort(IEnumerable<Policy> policies)
+    //    {
+    //        return policies.OrderBy(n => n.StartDate);
+    //    }
+    //}
+}
---
+++ SingLife.FacebookShareBonus.Tests/FacebookBonusCalculator_UnitTests.cs
@@ -0,0 +1,135 @@
+﻿using NUnit.Framework;
+using SingLife.FacebookShareBonus.Model;
+using System;
+
+namespace SingLife.FacebookShareBonus.Tests
+{
+    /// <summary>
+    /// Summary description for FacebookBonus
+    /// </summary>
+    [TestFixture]
+    public class FacebookBonusCalculator_UnitTests
+    {
+        public FacebookBonusCalculator_UnitTests()
+        {
+        }
+
+        public static FacebookBonusCalculationInput GetListSortingByDateWithSmallBonusOfPoint()
+        {
+            Policy policy1 = new Policy { PolicyNumber = "P001", Premium = 200, StartDate = new DateTime(2016, 5, 12) };
+            Policy policy2 = new Policy { PolicyNumber = "P002", Premium = 100, StartDate = new DateTime(2017, 11, 8) };
+            Policy[] policies = new Policy[] { policy1, policy2 };
+            FacebookBonusSettings facebookBonusSettings = new FacebookBonusSettings { BonusPercentage = 0.03F, MaximumBonus = 10 };
+            return new FacebookBonusCalculationInput { PoliciesOfCustomer = policies, Settings = facebookBonusSettings };
+        }
+
+        public static FacebookBonusCalculationInput GetListSortingByDate()
+        {
+            Policy policy1 = new Policy { PolicyNumber = "P001", Premium = 200, StartDate = new DateTime(2016, 5, 12) };
+            Policy policy2 = new Policy { PolicyNumber = "P002", Premium = 300, StartDate = new DateTime(2017, 11, 8) };
+            Policy[] policies = new Policy[] { policy1, policy2 };
+            FacebookBonusSettings facebookBonusSettings = new FacebookBonusSettings { BonusPercentage = 0.03F, MaximumBonus = 10 };
+            return new FacebookBonusCalculationInput { PoliciesOfCustomer = policies, Settings = facebookBonusSettings };
+        }
+
+        public static FacebookBonusCalculationInput GetListNotSortingByDateWithLargerBonusOfPoint()
+        {
+            //var sorter = A.Fake<IPolicySortService>();
+
+            Policy policy2 = new Policy { PolicyNumber = "P002", Premium = 300, StartDate = new DateTime(2017, 11, 8) };
+            Policy policy1 = new Policy { PolicyNumber = "P001", Premium = 200, StartDate = new DateTime(2016, 5, 12) };
+            Policy[] policies = new Policy[] { policy2, policy1 };
+
+            // Policy[] policies2 = A.Fake<Policy[]>();
+            //A.CallTo(() => sorter.Sort(policies)).Returns(policies2);
+
+            FacebookBonusSettings facebookBonusSettings = new FacebookBonusSettings { BonusPercentage = 0.03F, MaximumBonus = 10 };
+            return new FacebookBonusCalculationInput { PoliciesOfCustomer = policies, Settings = facebookBonusSettings };
+        }
+
+        public static FacebookBonusCalculationInput GetListNotSortingByDateWithSmallerBonusOfPoint()
+        {
+            //var sorter = A.Fake<IPolicySortService>();
+
+            Policy policy2 = new Policy { PolicyNumber = "P002", Premium = 100, StartDate = new DateTime(2017, 11, 8) };
+            Policy policy1 = new Policy { PolicyNumber = "P001", Premium = 200, StartDate = new DateTime(2016, 5, 12) };
+            Policy[] policies = new Policy[] { policy2, policy1 };
+
+            // Policy[] policies2 = A.Fake<Policy[]>();
+            //A.CallTo(() => sorter.Sort(policies)).Returns(policies2);
+
+            FacebookBonusSettings facebookBonusSettings = new FacebookBonusSettings { BonusPercentage = 0.03F, MaximumBonus = 10 };
+            return new FacebookBonusCalculationInput { PoliciesOfCustomer = policies, Settings = facebookBonusSettings };
+        }
+
+        //
+        // TODO: Add constructor logic here
+        //
+        [Test]
+        public void FacebookBonusCalculator_BonusInPointLargerThanMaximumBonus_TotalIsCorrect()
+        {
+            FacebookBonusCalculator facebookBonusCalculator = new FacebookBonusCalculator();
+            var output = facebookBonusCalculator.Calculate(GetListSortingByDate());
+
+            const int expectedTotal = 10;
+
+            Assert.AreEqual(expectedTotal, output.Total);
+        }
+
+        [Test]
+        public void FacebookBonusCalculator_BonusInPointSmallerThanMaximumBonus_TotalIsCorrect()
+        {
+            FacebookBonusCalculator facebookBonusCalculator = new FacebookBonusCalculator();
+            var output = facebookBonusCalculator.Calculate(GetListSortingByDateWithSmallBonusOfPoint());
+
+            const int expectedTotal = 9;
+
+
+            Assert.AreEqual(expectedTotal, output.Total);
+        }
+
+        [Test]
+        public void FacebookBonusCalculatorFacebookBonus_BonusInPointLargerThanMaximumBonus_ListPolicyBonusEqual()
+        {
+            FacebookBonusCalculator facebookBonusCalculator = new FacebookBonusCalculator();
+            var output = facebookBonusCalculator.Calculate(GetListSortingByDate());
+
+
+            const string expected = "P001 P002 10";
+
+            Assert.AreEqual(expected, output.ToString());
+        }
+
+        [Test]
+        public void FacebookBonusCalculator_BonusInPointSmallerThanMaximumBonus_ListPolicyBonusEqual()
+        {
+            FacebookBonusCalculator facebookBonusCalculator = new FacebookBonusCalculator();
+            var output = facebookBonusCalculator.Calculate(GetListSortingByDateWithSmallBonusOfPoint());
+
+            const string expected = "P001 P002 9";
+
+            Assert.AreEqual(expected, output.ToString());
+        }
+
+        [Test]
+        public void FacebookBonusCalculator_ListPolicyNotSortingByDateWithBonusLarger_TotalIsCorrect()
+        {
+            FacebookBonusCalculator facebookBonusCalculator = new FacebookBonusCalculator();
+            var output = facebookBonusCalculator.Calculate(GetListNotSortingByDateWithLargerBonusOfPoint());
+
+            const int expectedTotal = 10;
+
+            Assert.AreEqual(expectedTotal, output.Total);
+        }
+        [Test]
+        public void FacebookBonusCalculator_ListPolicyNotSortingByDateWithBonusSmaller_TotalIsCorrect()
+        {
+            FacebookBonusCalculator facebookBonusCalculator = new FacebookBonusCalculator();
+            var output = facebookBonusCalculator.Calculate(GetListNotSortingByDateWithSmallerBonusOfPoint());
+
+            const int expectedTotal = 9;
+
+            Assert.AreEqual(expectedTotal, output.Total);
+        }
+    }
+}
