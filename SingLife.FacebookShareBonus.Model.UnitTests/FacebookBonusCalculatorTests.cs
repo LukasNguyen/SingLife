4197d8ea8372ea7e8180391de249c31449c8ac42
CONTEMIVIETNAM\lukas.nguyen <lukas.nguyen@contemi.com.vn>
2018-01-05 15:23:59 +0700

    clean up unit tests

--- SingLife.FacebookShareBonus.Model.UnitTests/FacebookBonusCalculatorTests.cs
+++ SingLife.FacebookShareBonus.Model.UnitTests/FacebookBonusCalculatorTests.cs
@@ -1,7 +1,6 @@
 ﻿using FakeItEasy;
 using NUnit.Framework;
 using System;
-using System.Linq;
 
 namespace SingLife.FacebookShareBonus.Model.UnitTests
 {
@@ -9,157 +8,146 @@ namespace SingLife.FacebookShareBonus.Model.UnitTests
     public class FacebookBonusCalculatorTests
     {
         [Test]
-        public void FacebookBonusCalculator_BonusInPointSmallerThanMaximumBonus_TotalIsCorrect()
+        public void FacebookBonusCalculator_TotalBonusLessThanMaximumBonus_TotalIsCorrect()
         {
-            var facebookBonusCalculator = new FacebookBonusCalculator();
-            FacebookBonus facebookBonus = facebookBonusCalculator.Calculate(SampleInput(35));
-
-            int actual = facebookBonus.Total;
-
+            // Arrange
+            var facebookBonusCalculator = CreateCalculator();
+            var input = SampleInput(35);
             const int expectedTotal = 30;
 
-            Assert.AreEqual(expectedTotal, actual);
+            // Act
+            var facebookBonus = facebookBonusCalculator.Calculate(input);
+
+            // Assert
+            var actualTotal = facebookBonus.Total;
+            Assert.That(actualTotal, Is.EqualTo(expectedTotal));
         }
 
         [Test]
         public void FacebookBonusCalculator_TotalBonusLessThanMaximumBonus_PolicyBonusPointsAreCorrect()
         {
-            var facebookBonusCalculator = new FacebookBonusCalculator();
-            FacebookBonus facebookBonus = facebookBonusCalculator.Calculate(SampleInput(35));
+            // Arrange
+            var facebookBonusCalculator = CreateCalculator();
+            var input = SampleInput(35);
 
-            var actual = facebookBonus.PolicyBonuses.ToArray();
-
-            var policyBonus = new PolicyBonus
+            var expected = new[]
             {
-                PolicyNumber = "P001",
-                BonusInPoints = 6
+                new PolicyBonus { PolicyNumber = "P001", BonusInPoints = 6 },
+                new PolicyBonus { PolicyNumber = "P002", BonusInPoints = 9 },
+                new PolicyBonus { PolicyNumber = "P003", BonusInPoints = 6 },
+                new PolicyBonus { PolicyNumber = "P004", BonusInPoints = 9 }
             };
 
-            var policyBonus2 = new PolicyBonus
-            {
-                PolicyNumber = "P002",
-                BonusInPoints = 9
-            };
-            var policyBonus3 = new PolicyBonus
-            {
-                PolicyNumber = "P003",
-                BonusInPoints = 6
-            };
+            // Act
+            var facebookBonus = facebookBonusCalculator.Calculate(input);
 
-            var policyBonus4 = new PolicyBonus
-            {
-                PolicyNumber = "P004",
-                BonusInPoints = 9
-            };
-
-            PolicyBonus[] expected =
-            {
-                policyBonus,
-                policyBonus2,
-                policyBonus3,
-                policyBonus4
-            };
-
-            Assert.That(actual, Is.EqualTo(expected));
+            // Assert
+            var actual = facebookBonus.PolicyBonuses;
+            Assert.That(actual, Is.EqualTo(expected), "Policies should be rewarded with correct points.");
         }
 
         [Test]
         public void FacebookBonusCalculator_TotalBonusGreaterThanMaximumBonus_TotalIsCorrect()
         {
-            var facebookBonusCalculator = new FacebookBonusCalculator();
-            FacebookBonus facebookBonus = facebookBonusCalculator.Calculate(SampleInput(25));
-
-            int actual = facebookBonus.Total;
-
+            // Arrange
+            var facebookBonusCalculator = CreateCalculator();
             const int expectedTotal = 25;
+            var input = SampleInput(25);
 
-            Assert.AreEqual(expectedTotal, actual);
+            // Act
+            var facebookBonus = facebookBonusCalculator.Calculate(input);
+
+            // Assert
+            var actualTotal = facebookBonus.Total;
+            Assert.AreEqual(expectedTotal, actualTotal);
         }
 
         [Test]
-        public void FacebookBonusCalculator_TotalBonusGreaterThanMaximumBonus_LastPolicyBonusPointAreCorrect()
+        public void FacebookBonusCalculator_TotalBonusGreaterThanMaximumBonus_LastRewardedPolicyHasCorrectBonus()
         {
-            var facebookBonusCalculator = new FacebookBonusCalculator();
-            FacebookBonus facebookBonus = facebookBonusCalculator.Calculate(SampleInput(25));
-
-            var actual = facebookBonus.PolicyBonuses.Last();
-
+            // Arrange
+            var facebookBonusCalculator = CreateCalculator();
             var expected = new PolicyBonus
             {
-                PolicyNumber = "P004",
-                BonusInPoints = 4
+                PolicyNumber = "P002",
+                BonusInPoints = 6
             };
+            var input = SampleInput(12);
 
+            // Act
+            var facebookBonus = facebookBonusCalculator.Calculate(input);
+
+            // Assert
+            var actual = facebookBonus.PolicyBonuses[1];
             Assert.That(actual, Is.EqualTo(expected));
         }
 
         [Test]
         public void FacebookBonusCalculator_TotalBonusGreaterThanMaximumBonus_ExceededPoliciesWillHaveZeroPoints()
         {
-            var facebookBonusCalculator = new FacebookBonusCalculator();
-            FacebookBonus facebookBonus = facebookBonusCalculator.Calculate(SampleInput(14));
+            // Arrange
+            var facebookBonusCalculator = CreateCalculator();
 
-            var actual = facebookBonus.PolicyBonuses;
-
-            var policyBonus = new PolicyBonus
+            var expectedExceededPolicies = new[]
             {
-                PolicyNumber = "P003",
-                BonusInPoints = 0
+                new PolicyBonus
+                {
+                    PolicyNumber = "P003",
+                    BonusInPoints = 0
+                },
+                new PolicyBonus
+                {
+                    PolicyNumber = "P004",
+                    BonusInPoints = 0
+                }
             };
 
-            var policyBonus2 = new PolicyBonus
-            {
-                PolicyNumber = "P004",
-                BonusInPoints = 0
-            };
+            var input = SampleInput(14);
 
-            PolicyBonus[] expectedExceededPolicies =
-            {
-                policyBonus,
-                policyBonus2
-            };
+            // Act
+            var facebookBonus = facebookBonusCalculator.Calculate(input);
 
-            Assert.That(facebookBonus.PolicyBonuses, Is.SupersetOf(expectedExceededPolicies));
+            // Assert
+            var actualPolicies = facebookBonus.PolicyBonuses;
+            Assert.That(actualPolicies, Is.SupersetOf(expectedExceededPolicies), "Exceeded policies should not be rewarded.");
+        }
+
+        private static FacebookBonusCalculator CreateCalculator()
+        {
+            return new FacebookBonusCalculator();
         }
 
         private static FacebookBonusCalculationInput SampleInput(int maximumBonus)
         {
-            var policy1 = new Policy
+            var policies = new[]
             {
-                PolicyNumber = "P001",
-                Premium = 200,
-                StartDate = new DateTime(2016, 5, 12)
+                new Policy
+                {
+                    PolicyNumber = "P001",
+                    Premium = 200,
+                    StartDate = new DateTime(2016, 5, 12)
+                },
+                new Policy
+                {
+                    PolicyNumber = "P002",
+                    Premium = 300,
+                    StartDate = new DateTime(2017, 11, 8)
+                },
+                new Policy
+                {
+                    PolicyNumber = "P003",
+                    Premium = 200,
+                    StartDate = new DateTime(2017, 12, 12)
+                },
+                new Policy
+                {
+                    PolicyNumber = "P004",
+                    Premium = 300,
+                    StartDate = new DateTime(2017, 12, 15)
+                }
             };
 
-            var policy2 = new Policy
-            {
-                PolicyNumber = "P002",
-                Premium = 300,
-                StartDate = new DateTime(2017, 11, 8)
-            };
-
-            var policy3 = new Policy
-            {
-                PolicyNumber = "P003",
-                Premium = 200,
-                StartDate = new DateTime(2017, 12, 12)
-            };
-
-            var policy4 = new Policy
-            {
-                PolicyNumber = "P004",
-                Premium = 300,
-                StartDate = new DateTime(2017, 12, 15)
-            };
-
-            Policy[] policies = {
-                policy1,
-                policy2,
-                policy3,
-                policy4
-            };
-
-            IPolicySortService sorter = A.Fake<IPolicySortService>();
+            var sorter = A.Fake<IPolicySortService>();
             A.CallTo(() => sorter.Sort(policies)).Returns(policies);
 
             var facebookBonusSettings = new FacebookBonusSettings
